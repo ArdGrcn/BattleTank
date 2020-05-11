f6f2f502595981a3e7407637abdfd688cc3d389c
ArdGrcn <mr.ardagrcn@gmail.com>
2020-05-11 10:59:33 +0300

    Elevate function parameter name change, ATank no longer ticks

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -7,7 +7,7 @@
 ATank::ATank()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 	// No need to protect pointers as added in construction
 	TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming Component"));
@@ -25,14 +25,6 @@ void ATank::BeginPlay()
 {
 	Super::BeginPlay();
 	
-}
-
-
-// Called every frame
-void ATank::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
 }
 
 
