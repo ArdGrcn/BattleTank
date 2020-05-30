450eee7d4ad34c67a191f430c9789cec82add766
ArdGrcn <mr.ardagrcn@gmail.com>
2020-05-30 09:30:11 +0300

    Tank no longer slips, but it flies off whe it leaves the ground

--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -22,4 +22,10 @@ public:
     // Max force per track in newtons
     UPROPERTY(EditDefaultsOnly)
     float TrackMaxDrivingForce = 400000;
+
+private:
+    UTankTrack();
+
+    virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
 };
